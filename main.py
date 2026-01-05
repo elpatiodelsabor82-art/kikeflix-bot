b8d9d911ebeffdf869dc88f183b34c85fdb89f4e
elpatiodelsabor82-art <elpatiodelsabor.82@gmail.com>
2026-01-05 02:59:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from pyrogram import Client, filters
-app = Client("kike", api_id=32394929, api_hash="10e6543af1b09ee262f688f94f68ae71", bot_token="8560755206:AAGzqanMokL-r4gDqehH2p3qWMOwCPrRVIo")
-@app.on_message(filters.all)
-async def echo(client, message):
-    await message.reply_text("¡Aquí estoy! El bot está vivo.")
+
+# Datos directos para no fallar
+app = Client(
+    "kike_bot",
+    api_id=32394929,
+    api_hash="10e6543af1b09ee262f688f94f68ae71",
+    bot_token="8560755206:AAGzqanMokL-r4gDqehH2p3qWMOwCPrRVIo"
+)
+
+@app.on_message(filters.command("start"))
+async def start(client, message):
+    await message.reply_text("¡POR FIN! El bot está vivo y funcionando, Edgar.")
+
+print("Bot encendido...")
 app.run()
